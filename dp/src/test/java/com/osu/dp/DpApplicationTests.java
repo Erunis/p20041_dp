4f752b613191ac214b03d2cc423fcea7b67e1684
Gabriela Muchová <muchovagab@gmail.com>
2021-10-16 18:52:06 +0200

    Create initial SpringBoot application

---
+++ dp/src/test/java/com/osu/dp/DpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osu.dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
